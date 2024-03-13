6119233db30223a2fa0ddbc82e35ad8d94a89dd4
Kameron Bielawski <kam@EMCFs-MacBook-Pro-4.local>
2024-03-13 11:21:58 -0400

    update hillclimber

--- hillclimber.py
+++ hillclimber.py
@@ -6,7 +6,7 @@ from collections import Counter
 import numpy as np
 
 from afpo import Solution, WORLD_SIZE, NUM_STEPS, NUM_LAYERS, DEAD, ALIVE, ACTIVATION_SIGMOID, ACTIVATION_RELU, ACTIVATION_TANH
-from simulation import simulate, get_layer_mask, DEAD, ALIVE, WORLD_SIZE, NUM_STEPS, NUM_LAYERS, NUM_INPUT_NEURONS, NUM_OUTPUT_NEURONS, ACTIVATION_SIGMOID, ACTIVATION_RELU, ACTIVATION_TANH
+from simulation_cpu import simulate, get_layer_mask, DEAD, ALIVE, WORLD_SIZE, NUM_STEPS, NUM_LAYERS, NUM_INPUT_NEURONS, NUM_OUTPUT_NEURONS, ACTIVATION_SIGMOID, ACTIVATION_RELU, ACTIVATION_TANH
 from util import create_hollow_circle, create_square, create_diamond
 
 activation2int = {
@@ -25,6 +25,7 @@ class HillClimber:
         self.shape = experiment_constants['shape']
         self.mutate_layers = experiment_constants['mutate_layers']
         self.state_or_growth = experiment_constants['state_or_growth']
+        self.neighbor_map_type = experiment_constants['neighbor_map_type']
 
         self.n_layers = len(self.layers)
         self.base_layer = next((i for i, d in enumerate(self.layers) if d.get('base', False)), None)
@@ -32,6 +33,9 @@ class HillClimber:
         self.children_population = {}
         self.current_generation = 1
         self.num_ids = 0
+
+        self.below_map = self.initialize_below_map()
+        self.above_map = self.initialize_above_map()
 
         self.best_fitness_history = []
         self.mean_fitness_history = []
@@ -52,7 +56,7 @@ class HillClimber:
         start = time.perf_counter()
 
         unsimulated_growth_genotypes, unsimulated_state_genotypes, unsimulated_ids = self.get_unsimulated_genotypes()
-        init_phenotypes = self.make_seed_phenotypes(unsimulated_growth_genotypes.shape[0])
+        init_phenotypes = self.make_seed_phenotypes(self.target_population_size)
 
         rand_id = list(self.children_population.keys())[0]
 
@@ -67,7 +71,9 @@ class HillClimber:
             self.children_population[rand_id].above_start, 
             self.use_growth, 
             init_phenotypes, 
-            activation2int[self.activation])
+            activation2int[self.activation],
+            self.below_map,
+            self.above_map)
 
         elapsed = time.perf_counter() - start
         lps = self.target_population_size / elapsed
@@ -79,51 +85,43 @@ class HillClimber:
         for i, id in enumerate(unsimulated_ids):
             self.children_population[id].set_fitness(fitness_scores[i])
             self.children_population[id].set_simulated(True)
-            self.children_population[id].set_phenotype(phenotypes[i][-1][self.base_layer] > 0) # phenotype is now binarized last step of base layer
+            self.children_population[id].set_phenotype(phenotypes[i][self.base_layer][-1] > 0) # phenotype is now binarized last step of base layer
             # Get actual parent Solution object from population using parent_id
             parent_id = self.children_population[id].parent_id
             parent = self.parent_population[parent_id] if parent_id is not None else None
             if parent is not None:
                 parent_child_distances.append(self.children_population[id].get_distance_from_parent(parent))
 
-        if self.children_population[rand_id].parent_id is not None:
-            print(self.children_population[rand_id].mutation_info)
-            print(self.children_population[rand_id].state_genotype)
-            print(self.parent_population[self.children_population[rand_id].parent_id].state_genotype)
-            print(Counter([solution.mutation_info['layer'] for i, solution in self.children_population.items()]))
-            
-        print('Average fitness:',
-              np.mean([sol.fitness for id, sol in self.children_population.items()]),
-              ', Min fitness: ',
-              min([sol.fitness for id, sol in self.children_population.items()]))
-        print('Average age:',
-              np.mean([sol.age for id, sol in self.children_population.items()]))
         # Reduce the population by selecting parent or child to remove
         n_neutral_children = self.select()
-        self.n_neutral_over_generations.append(n_neutral_children)
         # Extend the population using tournament selection
         self.mutate_population()
 
+        print('Average fitness:',
+              np.mean([sol.fitness for id, sol in self.parent_population.items()]),
+              ', Min fitness: ',
+              min([sol.fitness for id, sol in self.parent_population.items()]))
+        print('Average age:',
+              np.mean([sol.age for id, sol in self.parent_population.items()]))
+        print('Proportion of neutral mutations: ', n_neutral_children / self.target_population_size)
+        
+        # if self.children_population[rand_id].parent_id is not None:
+        #     print(self.children_population[rand_id].mutation_info)
+        #     print(self.children_population[rand_id].state_genotype)
+        #     print(self.parent_population[self.children_population[rand_id].parent_id].state_genotype)
+        #     print(Counter([solution.mutation_info['layer'] for i, solution in self.children_population.items()]))
+            
+        self.n_neutral_over_generations.append(n_neutral_children)
         self.best_fitness_history.append(self.best_solution())
         self.mean_fitness_history.append(np.mean([sol.fitness for id, sol in self.parent_population.items()]))
         self.parent_child_distance_history.append(parent_child_distances)
+
 
     def initialize_population(self):
         # Initialize target_population_size random solutions
         for _ in range(self.target_population_size):
             new_id = self.get_available_id()
             self.children_population[new_id] = Solution(layers=self.layers, id=new_id)
-
-    def generate_new_individuals(self):
-        new_individuals = []
-        # 1 - Breed: do tournament selection
-        # The minus one is to make room for one random individual at the end.
-        for _ in range(self.target_population_size - 1):
-            # Randomly select an individual using tournament selection
-            parent = self.tournament_select()
-            new_individuals.append(parent.make_offspring(self.get_available_id()))
-
-        return new_individuals
 
     def select(self):
         """
@@ -155,14 +153,6 @@ class HillClimber:
         for id, solution in self.parent_population.items():
             child = solution.make_offspring(id, mutate_layers=self.mutate_layers, state_or_growth=self.state_or_growth)
             self.children_population[id] = child
-
-    def tournament_select(self):
-        """
-        Tournament selection randomly chooses two individuals from the population and
-        selects the better (based on a primary objective) of the two for reproduction/mutation
-        """
-        sol1, sol2 = np.random.choice(self.population, 2, replace=False)
-        return min(sol1, sol2)
 
 
     def get_unsimulated_genotypes(self):
@@ -198,9 +188,9 @@ class HillClimber:
             target = create_hollow_circle(target_size)
 
         # Upsize back to 64x64 because that's how we're comparing 
-        while target.shape[0] < 64: 
-            target = np.repeat(np.repeat(target, 2, axis=0), 2, axis=1)
-        assert target.shape[0] == 64
+        # while target.shape[0] < 64: 
+        #     target = np.repeat(np.repeat(target, 2, axis=0), 2, axis=1)
+        # assert target.shape[0] == 64
 
         return target
         
@@ -209,10 +199,10 @@ class HillClimber:
         """Score a set of phenotypes generated by the simulate function."""
         target = self.get_target_shape()
 
+        assert target.shape == (phenotypes[0][self.base_layer][-1]).shape
+
         # Infer pop_size from phenotypes
-        pop_size = phenotypes.shape[0]
-        # All phenotypes and the target image are WORLD_SIZE x WORLD_SIZE squares.
-        assert phenotypes.shape == (pop_size, NUM_STEPS, self.n_layers, WORLD_SIZE, WORLD_SIZE)
+        pop_size = len(phenotypes)
 
         # Allocate space for results.
         fitness_scores = np.zeros(pop_size, dtype=np.uint32)
@@ -222,12 +212,11 @@ class HillClimber:
             # Look at just the final state of the layer0 part of the phenotype.
             # Compare it to the target image, and sum up the deltas to get the
             # final fitness score (lower is better, 0 is a perfect score).
-            fitness_scores[i] = np.sum(np.abs(target - (phenotypes[i][-1][self.base_layer] > 0)))
+            fitness_scores[i] = np.sum(np.abs(target - (phenotypes[i][self.base_layer][-1] > 0)))
 
         return fitness_scores
 
 
-    # @functools.cache
     def make_seed_phenotypes(self, n):
         """Starting phenotypes to use by default (one ALIVE cell in middle)."""
         # For each inidividual, capture phenotype development over NUM_STEPS. Each
@@ -236,19 +225,50 @@ class HillClimber:
         # represent a sort of hierarchical internal state for the organism. Layers
         # 1 and 2 are conceptually smaller than layer0 (1/4 and 1/8 respectively),
         # but are represented using arrays of the same size for simplicity.
-        phenotypes = np.full(
-            (n, NUM_STEPS, self.n_layers, WORLD_SIZE, WORLD_SIZE),
-            DEAD, dtype=np.float32)
-        
-        middle_start = WORLD_SIZE // 2
-        middle_end = middle_start + self.layers[self.base_layer]['res']
-
-        # Use a single ALIVE pixel in the middle of the CA world as the initial
-        # phenotype state for all individuals in the population.
-        for i in range(n):
-            phenotypes[i][0][self.base_layer][middle_start:middle_end, middle_start:middle_end] = ALIVE
+        phenotypes = []
+        for _ in range(n):
+            phenotype = []
+            g = WORLD_SIZE 
+            for l in range(self.n_layers):
+                layer = np.zeros((NUM_STEPS,g,g))
+                phenotype.append(layer)
+                if l == self.base_layer:
+                    phenotype[self.base_layer][0,g//2,g//2] = ALIVE
+                g = g // 2
+            phenotypes.append(phenotype)
 
         return phenotypes
+    
+    def initialize_below_map(self):
+        below_map = np.zeros((self.n_layers, 4, 3)).astype(int)
+        if self.neighbor_map_type == 'random':
+            for l in range(self.n_layers):
+                rand_l = np.random.randint(self.n_layers)
+                rand_r_offset = np.random.randint(WORLD_SIZE)
+                rand_c_offset = np.random.randint(WORLD_SIZE)
+                below_map[l, 0] = [rand_l, rand_r_offset, rand_c_offset]
+        else:
+            for l in range(self.n_layers):
+                below_map[l, 0] = [l-1, 0, 0]
+                below_map[l, 1] = [l-1, 0, 1]
+                below_map[l, 2] = [l-1, 1, 0]
+                below_map[l, 3] = [l-1, 1, 1]
+
+        return below_map
+    
+    def initialize_above_map(self):
+        above_map = np.zeros((self.n_layers, 3)).astype(int)
+        if self.neighbor_map_type == 'random':
+            for l in range(self.n_layers):
+                rand_l = np.random.randint(self.n_layers)
+                rand_r_offset = np.random.randint(WORLD_SIZE)
+                rand_c_offset = np.random.randint(WORLD_SIZE)
+                above_map[l] = [rand_l, rand_r_offset, rand_c_offset]
+        else:
+            for l in range(self.n_layers):
+                above_map[l] = [l+1, 0, 0]
+
+        return above_map
 
 
     def pickle_hc(self, pickle_file_name):
